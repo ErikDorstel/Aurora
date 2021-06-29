315e007c9e54baa8ab74b8da10373ae51c3c5359
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2021-06-29 20:05:49 +0200

    add sweep control

--- index_html.h
+++ index_html.h
@@ -12,12 +12,16 @@ html     { font-family: Helvetica; display: inline-block; margin: 0px auto; text
 <script>
 
 function SSTC2init() {
-  sstc2SpeedArray=[1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,75,100,200,500]; sstc2Speed=0; sstc2Start=0; sstc2Power=0; sstc2FreqA=50; sstc2FreqB=50; sstc2Ratio=180;
+  sstc2SpeedArray=[1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,75,100,200,500]; sstc2Speed=0; sstc2Start=0; sstc2Power=0;
+  sstc2SweepA=0; sstc2SweepB=5000; sstc2SweepDur=3000; sstc2FreqA=50; sstc2FreqB=50; sstc2Ratio=180;
   document.getElementById("speedBtn").innerHTML="Speed "+Math.round(5000/sstc2SpeedArray[sstc2Speed])/100+" Hz";
   document.getElementById("startBtn").innerHTML="Start "+sstc2Start/1000+" ms";
   document.getElementById("powerBtn").innerHTML="Power "+sstc2Power/1000+" ms";
-  document.getElementById("freqaBtn").innerHTML="Frequency A "+sstc2FreqA;
-  document.getElementById("freqbBtn").innerHTML="Frequency B "+sstc2FreqB;
+  document.getElementById("sweepaBtn").innerHTML="Sweep A "+sstc2SweepA+" Hz";
+  document.getElementById("sweepbBtn").innerHTML="Sweep B "+sstc2SweepB+" Hz";
+  document.getElementById("sweepdurBtn").innerHTML="Duration "+sstc2SweepDur+" ms";
+  document.getElementById("freqaBtn").innerHTML="Frequency A "+sstc2FreqA+" Hz";
+  document.getElementById("freqbBtn").innerHTML="Frequency B "+sstc2FreqB+" Hz";
   document.getElementById("ratioBtn").innerHTML="Ratio "+sstc2Ratio; }
 
 function speedDef() { sstc2Speed=0; doRangeSync(0); }
@@ -29,19 +33,30 @@ function startInc() { sstc2Start+=500; doRangeSync(1); }
 function powerDef() { sstc2Power=0; doRangeSync(0); }
 function powerDec() { sstc2Power-=500; doRangeSync(0); }
 function powerInc() { sstc2Power+=500; doRangeSync(0); }
-function freqaDef() { sstc2FreqA=50; doRangeAsync(); }
-function freqaDec() { sstc2FreqA-=1; doRangeAsync(); }
-function freqaInc() { sstc2FreqA+=1; doRangeAsync(); }
-function freqaDec10() { sstc2FreqA-=10; doRangeAsync(); }
-function freqaInc10() { sstc2FreqA+=10; doRangeAsync(); }
-function freqbDef() { sstc2FreqB=sstc2FreqA; doRangeAsync(); }
-function freqbDec() { sstc2FreqB-=1; doRangeAsync(); }
-function freqbInc() { sstc2FreqB+=1; doRangeAsync(); }
-function freqbDec10() { sstc2FreqB-=10; doRangeAsync(); }
-function freqbInc10() { sstc2FreqB+=10; doRangeAsync(); }
-function ratioDef() { sstc2Ratio=180; doRangeAsync(); }
-function ratioDec() { sstc2Ratio-=15; doRangeAsync(); }
-function ratioInc() { sstc2Ratio+=15; doRangeAsync(); }
+
+function sweepaDef() { sstc2SweepA=0; doRangeAsync(0); }
+function sweepaDec() { sstc2SweepA-=250; doRangeAsync(0); }
+function sweepaInc() { sstc2SweepA+=250; doRangeAsync(0); }
+function sweepbDef() { sstc2SweepB=5000; doRangeAsync(0); }
+function sweepbDec() { sstc2SweepB-=250; doRangeAsync(0); }
+function sweepbInc() { sstc2SweepB+=250; doRangeAsync(0); }
+function sweepdurDef() { sstc2SweepDur=3000; doRangeAsync(0); }
+function sweepdurDec() { sstc2SweepDur-=250; doRangeAsync(0); }
+function sweepdurInc() { sstc2SweepDur+=250; doRangeAsync(0); }
+
+function freqaDef() { sstc2FreqA=50; doRangeAsync(1); }
+function freqaDec() { sstc2FreqA-=1; doRangeAsync(1); }
+function freqaInc() { sstc2FreqA+=1; doRangeAsync(1); }
+function freqaDec10() { sstc2FreqA-=10; doRangeAsync(1); }
+function freqaInc10() { sstc2FreqA+=10; doRangeAsync(1); }
+function freqbDef() { sstc2FreqB=sstc2FreqA; doRangeAsync(1); }
+function freqbDec() { sstc2FreqB-=1; doRangeAsync(1); }
+function freqbInc() { sstc2FreqB+=1; doRangeAsync(1); }
+function freqbDec10() { sstc2FreqB-=10; doRangeAsync(1); }
+function freqbInc10() { sstc2FreqB+=10; doRangeAsync(1); }
+function ratioDef() { sstc2Ratio=180; doRangeAsync(1); }
+function ratioDec() { sstc2Ratio-=15; doRangeAsync(1); }
+function ratioInc() { sstc2Ratio+=15; doRangeAsync(1); }
 
 function toneOff() { sendAJAX("setSSTC2Tone,0,0,0,0"); }
 function toneC() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((60-69)/12))*440+",0,"+sstc2Ratio+",0"); }
@@ -53,8 +68,8 @@ function toneA() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((69-69)/12))*440+",0,
 function toneH() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((71-69)/12))*440+",0,"+sstc2Ratio+",0"); }
 function toneC1() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((72-69)/12))*440+",0,"+sstc2Ratio+",0"); }
 
-function sweepUp() { sendAJAX("setSSTC2Sweep,100,5000,3000,"+sstc2Ratio); }
-function sweepDown() { sendAJAX("setSSTC2Sweep,5000,100,3000,"+sstc2Ratio); }
+function sweepUp() { sendAJAX("setSSTC2Sweep,"+sstc2SweepA+","+sstc2SweepB+","+sstc2SweepDur+","+sstc2Ratio); }
+function sweepDown() { sendAJAX("setSSTC2Sweep,"+sstc2SweepB+","+sstc2SweepA+","+sstc2SweepDur+","+sstc2Ratio); }
 
 function doRangeSync(start) {
   if (sstc2Speed<0) { sstc2Speed=0; } if (sstc2Speed>19) { sstc2Speed=19; }
@@ -67,16 +82,25 @@ function doRangeSync(start) {
   sendAJAX("setSSTC2Para"+","+sstc2SpeedArray[sstc2Speed]+","+sstc2Start+","+sstc2Power); }
 
 function doRangeAsync(start) {
+  if (sstc2SweepA<0) { sstc2SweepA=0; }
+  if (sstc2SweepA>5000) { sstc2SweepA=5000; }
+  if (sstc2SweepB<0) { sstc2SweepB=0; }
+  if (sstc2SweepB>5000) { sstc2SweepB=5000; }
+  if (sstc2SweepDur<250) { sstc2SweepDur=250; }
+  if (sstc2SweepDur>5000) { sstc2SweepDur=5000; }
   if (sstc2FreqA<0) { sstc2FreqA=0; }
   if (sstc2FreqA>200) { sstc2FreqA=200; }
   if (sstc2FreqB<0) { sstc2FreqB=0; }
   if (sstc2FreqB>200) { sstc2FreqB=200; }
   if (sstc2Ratio<0) { sstc2Ratio=0; }
   if (sstc2Ratio>255) { sstc2Ratio=255; }
-  document.getElementById("freqaBtn").innerHTML="Frequency A "+sstc2FreqA;
-  document.getElementById("freqbBtn").innerHTML="Frequency B "+sstc2FreqB;
+  document.getElementById("sweepaBtn").innerHTML="Sweep A "+sstc2SweepA+" Hz";
+  document.getElementById("sweepbBtn").innerHTML="Sweep B "+sstc2SweepB+" Hz";
+  document.getElementById("sweepdurBtn").innerHTML="Duration "+sstc2SweepDur+" ms";
+  document.getElementById("freqaBtn").innerHTML="Frequency A "+sstc2FreqA+" Hz";
+  document.getElementById("freqbBtn").innerHTML="Frequency B "+sstc2FreqB+" Hz";
   document.getElementById("ratioBtn").innerHTML="Ratio "+sstc2Ratio;
-  sendAJAX("setSSTC2Tone"+","+sstc2FreqA+","+sstc2FreqB+","+sstc2Ratio+","+sstc2Ratio); }
+  if (start==1) { sendAJAX("setSSTC2Tone"+","+sstc2FreqA+","+sstc2FreqB+","+sstc2Ratio+","+sstc2Ratio); } }
 
 function sendAJAX(value) {
   if (window.XMLHttpRequest) { ajaxObj=new XMLHttpRequest; } else if (window.ActiveXObject) { ajaxObj=new ActiveXObject("Microsoft.XMLHTTP"); }
@@ -105,6 +129,15 @@ function sendAJAX(value) {
 <p><button class="button" ontouchstart="toneH();" ontouchend="toneOff();">H</button>
    <button class="button" ontouchstart="toneC1();" ontouchend="toneOff();">C1</button></p>
 
+<p><button class="button" id="sweepaBtn" onclick="sweepaDef();"></button></p>
+<p><button class="button button2" onclick="sweepaDec();">&#8722;</button>
+   <button class="button button2" onclick="sweepaInc();">+</button></p>
+<p><button class="button" id="sweepbBtn" onclick="sweepbDef();"></button></p>
+<p><button class="button button2" onclick="sweepbDec();">&#8722;</button>
+   <button class="button button2" onclick="sweepbInc();">+</button></p>
+<p><button class="button" id="sweepdurBtn" onclick="sweepdurDef();"></button></p>
+<p><button class="button button2" onclick="sweepdurDec();">&#8722;</button>
+   <button class="button button2" onclick="sweepdurInc();">+</button></p>
 <p><button class="button" onclick="sweepUp();">Sweep Up</button></p>
 <p><button class="button" onclick="sweepDown();">Sweep Down</button></p>
 
