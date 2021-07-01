8e0f747b0c0386d0bfc9dadcefe177d2a5a1cb23
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2021-07-01 09:36:02 +0200

    migrate from ledc to native timers

--- SSTC-2.h
+++ SSTC-2.h
@@ -1,53 +1,53 @@
 // SSTC2 Library
 // 2021 Erik Dorstel
 // call "initSSTC2();" first
-// call "modeSSTC2sync();" or "modeSSTC2async();" second
 // call "setSSTC2Para(speed,start,power);" to set parameters for sync mode
-// call "setSSTC2Tone(freq,power);" to set parameters for async mode
+// call "setSSTC2Tone(freqA,freqB,ratioA,ratioB);" to set parameters for async mode
+// call "setSSTC2Sweep(freqA,freqB,duration,ratio);" to set parameters for async mode
 
 const byte onBoardLED=2, phaseIn=18, phaseOut=19; byte SSTC2mode=0;
-hw_timer_t * timer1 = NULL; hw_timer_t * timer2 = NULL; hw_timer_t * timer3 = NULL;
+hw_timer_t * timer1 = NULL; hw_timer_t * timer2 = NULL; hw_timer_t * timer3 = NULL; hw_timer_t * timer4 = NULL;
 
 void IRAM_ATTR onPhase() { detachInterrupt(phaseIn); timerRestart(timer1); timerAlarmEnable(timer1); timerRestart(timer2); timerAlarmEnable(timer2); }
-void IRAM_ATTR onTimer1() { attachInterrupt(phaseIn,onPhase,RISING); }
-void IRAM_ATTR onTimer2() { digitalWrite(phaseOut,LOW); digitalWrite(onBoardLED,HIGH); timerRestart(timer3); timerAlarmEnable(timer3); }
-void IRAM_ATTR onTimer3() { digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); }
+void IRAM_ATTR onTimerSync1() { attachInterrupt(phaseIn,onPhase,RISING); }
+void IRAM_ATTR onTimerSync2() { digitalWrite(phaseOut,LOW); digitalWrite(onBoardLED,HIGH); timerRestart(timer3); timerAlarmEnable(timer3); }
+void IRAM_ATTR onTimerSync3() { digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); }
+void IRAM_ATTR onTimerAsync1() { digitalWrite(phaseOut,LOW); digitalWrite(onBoardLED,HIGH); timerRestart(timer2); timerAlarmEnable(timer2); }
+void IRAM_ATTR onTimerAsync2() { digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); }
+void IRAM_ATTR onTimerAsync3() { digitalWrite(phaseOut,LOW); digitalWrite(onBoardLED,HIGH); timerRestart(timer4); timerAlarmEnable(timer4); }
+void IRAM_ATTR onTimerAsync4() { digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); }
 
 void initSSTC2() {
   pinMode(onBoardLED,OUTPUT); pinMode(phaseIn,INPUT); pinMode(phaseOut,OUTPUT);
   digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW);
-  // controls speed by masking various cycles
-  timer1=timerBegin(0,80,true); timerAttachInterrupt(timer1,&onTimer1,true); timerAlarmWrite(timer1,19000,false);
-  // controls start by adjusting enable timepoint
-  timer2=timerBegin(1,80,true); timerAttachInterrupt(timer2,&onTimer2,true); timerAlarmWrite(timer2,0,false);
-  // controls power by adjusting disable timepoint
-  timer3=timerBegin(2,80,true); timerAttachInterrupt(timer3,&onTimer3,true); timerAlarmWrite(timer3,0,false); }
+  timer1=timerBegin(0,80,true); timer2=timerBegin(1,80,true); timer3=timerBegin(2,80,true); timer4=timerBegin(3,80,true); }
 
 void modeSSTC2sync() {
-  ledcDetachPin(phaseOut); delay(30);
+  timerAlarmDisable(timer1); timerAlarmDisable(timer2); timerAlarmDisable(timer3); timerAlarmDisable(timer4); delay(30);
   digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW);
-  // trigger on rising edge of input sync signal
-  attachInterrupt(phaseIn,onPhase,RISING); }
+  timerAttachInterrupt(timer1,&onTimerSync1,true); timerAlarmWrite(timer1,19000,false); // controls speed by masking various cycles
+  timerAttachInterrupt(timer2,&onTimerSync2,true); timerAlarmWrite(timer2,0,false); // controls start by adjusting enable timepoint
+  timerAttachInterrupt(timer3,&onTimerSync3,true); timerAlarmWrite(timer3,0,false); // controls power by adjusting disable timepoint
+  attachInterrupt(phaseIn,onPhase,RISING); } // trigger on rising edge of input sync signal
   
 void modeSSTC2async() {
-  detachInterrupt(phaseIn); timerAlarmDisable(timer1); delay(30);
-  digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); }
+  detachInterrupt(phaseIn); timerAlarmDisable(timer1); timerAlarmDisable(timer2); timerAlarmDisable(timer3); timerAlarmDisable(timer4); delay(30);
+  digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW);
+  timerAttachInterrupt(timer1,&onTimerAsync1,true); timerAlarmWrite(timer1,0,true); // controls tone a frequency
+  timerAttachInterrupt(timer2,&onTimerAsync2,true); timerAlarmWrite(timer2,0,false); // controls tone a ratio
+  timerAttachInterrupt(timer3,&onTimerAsync3,true); timerAlarmWrite(timer3,0,true); // controls tone b frequency
+  timerAttachInterrupt(timer4,&onTimerAsync4,true); timerAlarmWrite(timer4,0,false); } // controls tone b ratio
 
 void setSSTC2Para(int speed,int start,int power) {
   if (SSTC2mode!=1) { modeSSTC2sync(); SSTC2mode=1; }
-  timerAlarmWrite(timer1,(speed*20000)-1000,false);
-  timerAlarmWrite(timer2,start,false);
-  timerAlarmWrite(timer3,power,false); }
+  timerAlarmWrite(timer1,(speed*20000)-1000,false); timerAlarmWrite(timer2,start,false); timerAlarmWrite(timer3,power,false); }
 
 void setSSTC2Tone(int freqA, int freqB, int ratioA, int ratioB) {
-  if (SSTC2mode!=2) { modeSSTC2async(); SSTC2mode=2; }
-  if (freqA==0 & freqB==0) { ledcDetachPin(phaseOut); digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); } else {
-    // uses channel 0 on timer 0
-    if (freqA>0) { ledcSetup(0,freqA,8); ledcAttachPin(phaseOut,0); ledcWrite(0,255-ratioA); digitalWrite(onBoardLED,HIGH); }
-    // uses channel 2 on timer 1
-    if (freqB>0) { ledcSetup(2,freqB,8); ledcAttachPin(phaseOut,2); ledcWrite(2,255-ratioB); digitalWrite(onBoardLED,HIGH); } } }
+  if (SSTC2mode!=2) { modeSSTC2async(); SSTC2mode=2; } if (ratioA==100) { ratioA=99; } if (ratioB==100) { ratioB=99; }
+  if (freqA>0 & ratioA>0) { timerAlarmWrite(timer1,1000000/freqA,true); timerAlarmWrite(timer2,10000*ratioA/freqA,false); timerRestart(timer1); timerAlarmEnable(timer1); } else { timerAlarmDisable(timer1); }
+  if (freqB>0 & ratioB>0) { timerAlarmWrite(timer3,1000000/freqB,true); timerAlarmWrite(timer4,10000*ratioB/freqB,false); timerRestart(timer3); timerAlarmEnable(timer3); } else { timerAlarmDisable(timer3); } }
 
 void setSSTC2Sweep(int freq1, int freq2, int duration, int ratio) {
-  if (freq1<freq2) { for (int freq=freq1;freq<=freq2;freq+=(freq2-freq1)*20/duration) { setSSTC2Tone(freq,0,ratio,0); delay(20); } setSSTC2Tone(0,0,0,0); }
-  if (freq1>freq2) { for (int freq=freq1;freq>=freq2;freq-=(freq1-freq2)*20/duration) { setSSTC2Tone(freq,0,ratio,0); delay(20); } setSSTC2Tone(0,0,0,0); } }
+  if (freq1<freq2 & duration>0) { for (int freq=freq1;freq<=freq2;freq+=(freq2-freq1)*20/duration) { setSSTC2Tone(freq,0,ratio,0); delay(20); } setSSTC2Tone(0,0,0,0); }
+  if (freq1>freq2 & duration>0) { for (int freq=freq1;freq>=freq2;freq-=(freq1-freq2)*20/duration) { setSSTC2Tone(freq,0,ratio,0); delay(20); } setSSTC2Tone(0,0,0,0); } }
   
--- index_html.h
+++ index_html.h
@@ -16,16 +16,18 @@ div  { background-color:#888888; color:#ffffff; border:0px; padding:0px; margin:
 
 function SSTC2init() {
   sstc2SpeedArray=[1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,75,100,200,500]; sstc2Speed=0; sstc2Start=0; sstc2Power=0;
-  sstc2SweepA=0; sstc2SweepB=5000; sstc2SweepDur=3000; sstc2FreqA=50; sstc2FreqB=50; sstc2Ratio=180;
+  sstc2Octave=1; sstc2SweepA=0; sstc2SweepB=5000; sstc2SweepDur=3000; sstc2FreqA=50; sstc2FreqB=50; sstc2RatioA=50; sstc2RatioB=50;
   document.getElementById("speedBtn").innerHTML="Speed "+Math.round(5000/sstc2SpeedArray[sstc2Speed])/100+" Hz";
   document.getElementById("startBtn").innerHTML="Start "+sstc2Start/1000+" ms";
   document.getElementById("powerBtn").innerHTML="Power "+sstc2Power/1000+" ms";
+  document.getElementById("octaveBtn").innerHTML="Octave "+sstc2Octave;
   document.getElementById("sweepaBtn").innerHTML="Sweep A "+sstc2SweepA+" Hz";
   document.getElementById("sweepbBtn").innerHTML="Sweep B "+sstc2SweepB+" Hz";
   document.getElementById("sweepdurBtn").innerHTML="Duration "+sstc2SweepDur+" ms";
   document.getElementById("freqaBtn").innerHTML="Frequency A "+sstc2FreqA+" Hz";
   document.getElementById("freqbBtn").innerHTML="Frequency B "+sstc2FreqB+" Hz";
-  document.getElementById("ratioBtn").innerHTML="Ratio "+sstc2Ratio; }
+  document.getElementById("ratioaBtn").innerHTML="Ratio "+sstc2RatioA;
+  document.getElementById("ratiobBtn").innerHTML="Ratio "+sstc2RatioB; }
 
 function speedDef() { sstc2Speed=0; doRangeSync(0); }
 function speedDec() { sstc2Speed+=1; doRangeSync(0); }
@@ -36,6 +38,10 @@ function startInc() { sstc2Start+=500; doRangeSync(1); }
 function powerDef() { sstc2Power=0; doRangeSync(0); }
 function powerDec() { sstc2Power-=500; doRangeSync(0); }
 function powerInc() { sstc2Power+=500; doRangeSync(0); }
+
+function octaveDef() { sstc2Octave=1; doRangeAsync(0); }
+function octaveDec() { sstc2Octave-=1; doRangeAsync(0); }
+function octaveInc() { sstc2Octave+=1; doRangeAsync(0); }
 
 function sweepaDef() { sstc2SweepA=0; doRangeAsync(0); }
 function sweepaDec() { sstc2SweepA-=250; doRangeAsync(0); }
@@ -57,22 +63,25 @@ function freqbDec() { sstc2FreqB-=1; doRangeAsync(1); }
 function freqbInc() { sstc2FreqB+=1; doRangeAsync(1); }
 function freqbDec10() { sstc2FreqB-=10; doRangeAsync(1); }
 function freqbInc10() { sstc2FreqB+=10; doRangeAsync(1); }
-function ratioDef() { sstc2Ratio=180; doRangeAsync(1); }
-function ratioDec() { sstc2Ratio-=15; doRangeAsync(1); }
-function ratioInc() { sstc2Ratio+=15; doRangeAsync(1); }
+function ratioaDef() { sstc2RatioA=50; doRangeAsync(1); }
+function ratioaDec() { sstc2RatioA-=5; doRangeAsync(1); }
+function ratioaInc() { sstc2RatioA+=5; doRangeAsync(1); }
+function ratiobDef() { sstc2RatioB=50; doRangeAsync(1); }
+function ratiobDec() { sstc2RatioB-=5; doRangeAsync(1); }
+function ratiobInc() { sstc2RatioB+=5; doRangeAsync(1); }
 
 function toneOff() { sendAJAX("setSSTC2Tone,0,0,0,0"); }
-function toneC() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((60-69)/12))*440+",0,"+sstc2Ratio+",0"); }
-function toneD() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((62-69)/12))*440+",0,"+sstc2Ratio+",0"); }
-function toneE() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((64-69)/12))*440+",0,"+sstc2Ratio+",0"); }
-function toneF() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((65-69)/12))*440+",0,"+sstc2Ratio+",0"); }
-function toneG() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((67-69)/12))*440+",0,"+sstc2Ratio+",0"); }
-function toneA() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((69-69)/12))*440+",0,"+sstc2Ratio+",0"); }
-function toneH() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((71-69)/12))*440+",0,"+sstc2Ratio+",0"); }
-function toneC1() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,((72-69)/12))*440+",0,"+sstc2Ratio+",0"); }
+function toneC() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,(-9/12)+sstc2Octave)*220+",0,"+sstc2RatioA+",0"); }
+function toneD() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,(-7/12)+sstc2Octave)*220+",0,"+sstc2RatioA+",0"); }
+function toneE() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,(-5/12)+sstc2Octave)*220+",0,"+sstc2RatioA+",0"); }
+function toneF() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,(-4/12)+sstc2Octave)*220+",0,"+sstc2RatioA+",0"); }
+function toneG() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,(-2/12)+sstc2Octave)*220+",0,"+sstc2RatioA+",0"); }
+function toneA() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,(0/12)+sstc2Octave)*220+",0,"+sstc2RatioA+",0"); }
+function toneH() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,(2/12)+sstc2Octave)*220+",0,"+sstc2RatioA+",0"); }
+function toneCn() { sendAJAX("setSSTC2Tone"+","+Math.pow(2,(3/12)+sstc2Octave)*220+",0,"+sstc2RatioA+",0"); }
 
-function sweepUp() { sendAJAX("setSSTC2Sweep,"+sstc2SweepA+","+sstc2SweepB+","+sstc2SweepDur+","+sstc2Ratio); }
-function sweepDown() { sendAJAX("setSSTC2Sweep,"+sstc2SweepB+","+sstc2SweepA+","+sstc2SweepDur+","+sstc2Ratio); }
+function sweepUp() { sendAJAX("setSSTC2Sweep,"+sstc2SweepA+","+sstc2SweepB+","+sstc2SweepDur+","+sstc2RatioA); }
+function sweepDown() { sendAJAX("setSSTC2Sweep,"+sstc2SweepB+","+sstc2SweepA+","+sstc2SweepDur+","+sstc2RatioA); }
 
 function doRangeSync(start) {
   if (sstc2Speed<0) { sstc2Speed=0; } if (sstc2Speed>19) { sstc2Speed=19; }
@@ -85,25 +94,31 @@ function doRangeSync(start) {
   sendAJAX("setSSTC2Para"+","+sstc2SpeedArray[sstc2Speed]+","+sstc2Start+","+sstc2Power); }
 
 function doRangeAsync(start) {
+  if (sstc2Octave<0) { sstc2Octave=0; }
+  if (sstc2Octave>4) { sstc2Octave=4; }
   if (sstc2SweepA<0) { sstc2SweepA=0; }
   if (sstc2SweepA>5000) { sstc2SweepA=5000; }
   if (sstc2SweepB<0) { sstc2SweepB=0; }
   if (sstc2SweepB>5000) { sstc2SweepB=5000; }
-  if (sstc2SweepDur<250) { sstc2SweepDur=250; }
+  if (sstc2SweepDur<0) { sstc2SweepDur=0; }
   if (sstc2SweepDur>5000) { sstc2SweepDur=5000; }
   if (sstc2FreqA<0) { sstc2FreqA=0; }
   if (sstc2FreqA>200) { sstc2FreqA=200; }
   if (sstc2FreqB<0) { sstc2FreqB=0; }
   if (sstc2FreqB>200) { sstc2FreqB=200; }
-  if (sstc2Ratio<0) { sstc2Ratio=0; }
-  if (sstc2Ratio>255) { sstc2Ratio=255; }
+  if (sstc2RatioA<0) { sstc2RatioA=0; }
+  if (sstc2RatioA>100) { sstc2RatioA=100; }
+  if (sstc2RatioB<0) { sstc2RatioB=0; }
+  if (sstc2RatioB>100) { sstc2RatioB=100; }
+  document.getElementById("octaveBtn").innerHTML="Octave "+sstc2Octave;
   document.getElementById("sweepaBtn").innerHTML="Sweep A "+sstc2SweepA+" Hz";
   document.getElementById("sweepbBtn").innerHTML="Sweep B "+sstc2SweepB+" Hz";
   document.getElementById("sweepdurBtn").innerHTML="Duration "+sstc2SweepDur+" ms";
   document.getElementById("freqaBtn").innerHTML="Frequency A "+sstc2FreqA+" Hz";
   document.getElementById("freqbBtn").innerHTML="Frequency B "+sstc2FreqB+" Hz";
-  document.getElementById("ratioBtn").innerHTML="Ratio "+sstc2Ratio;
-  if (start==1) { sendAJAX("setSSTC2Tone"+","+sstc2FreqA+","+sstc2FreqB+","+sstc2Ratio+","+sstc2Ratio); } }
+  document.getElementById("ratioaBtn").innerHTML="Ratio "+sstc2RatioA;
+  document.getElementById("ratiobBtn").innerHTML="Ratio "+sstc2RatioB;
+  if (start==1) { sendAJAX("setSSTC2Tone"+","+sstc2FreqA+","+sstc2FreqB+","+sstc2RatioA+","+sstc2RatioB); } }
 
 function sendAJAX(value) {
   if (window.XMLHttpRequest) { ajaxObj=new XMLHttpRequest; } else if (window.ActiveXObject) { ajaxObj=new ActiveXObject("Microsoft.XMLHTTP"); }
@@ -123,6 +138,11 @@ function sendAJAX(value) {
 <div><div class="x2" onclick="powerDec();">&#8722;</div>
      <div class="x2" onclick="powerInc();">+</div></div>
 
+<div><div class="x0">Play Note</div></div>
+
+<div><div class="x1" id="octaveBtn" onclick="octaveDef();"></div></div>
+<div><div class="x2" onclick="octaveDec();">&#8722;</div>
+     <div class="x2" onclick="octaveInc();">+</div></div>
 <div><div class="x4" ontouchstart="toneC();" ontouchend="toneOff();">C</div>
      <div class="x4" ontouchstart="toneD();" ontouchend="toneOff();">D</div>
      <div class="x4" ontouchstart="toneE();" ontouchend="toneOff();">E</div>
@@ -130,7 +150,9 @@ function sendAJAX(value) {
 <div><div class="x4" ontouchstart="toneG();" ontouchend="toneOff();">G</div>
      <div class="x4" ontouchstart="toneA();" ontouchend="toneOff();">A</div>
      <div class="x4" ontouchstart="toneH();" ontouchend="toneOff();">H</div>
-     <div class="x4" ontouchstart="toneC1();" ontouchend="toneOff();">C1</div></div>
+     <div class="x4" ontouchstart="toneCn();" ontouchend="toneOff();">C</div></div>
+
+<div><div class="x0">Play Sweep</div></div>
 
 <div><div class="x1" id="sweepaBtn" onclick="sweepaDef();"></div></div>
 <div><div class="x2" onclick="sweepaDec();">&#8722;</div>
@@ -144,7 +166,8 @@ function sendAJAX(value) {
 <div><div class="x2" onclick="sweepUp();">Sweep Up</div>
      <div class="x2" onclick="sweepDown();">Sweep Down</div></div>
 
-<div><div class="x1" onclick="toneOff();">Tone Off</div></div>
+<div><div class="x0">Beating</div></div>
+
 <div><div class="x1" id="freqaBtn" onclick="freqaDef();"></div></div>
 <div><div class="x4" onclick="freqaDec10();">&#8722;10</div>
      <div class="x4" onclick="freqaDec();">&#8722;1</div>
@@ -155,9 +178,17 @@ function sendAJAX(value) {
      <div class="x4" onclick="freqbDec();">&#8722;1</div>
      <div class="x4" onclick="freqbInc();">+1</div>
      <div class="x4" onclick="freqbInc10();">+10</div></div>
-<div><div class="x1" id="ratioBtn" onclick="ratioDef();"></div></div>
-<div><div class="x2" onclick="ratioDec();">&#8722;</div>
-     <div class="x2" onclick="ratioInc();">+</div></div>
+<div><div class="x2" onclick="doRangeAsync(1);">Tone On</div>
+     <div class="x2" onclick="toneOff();">Tone Off</div></div>
+
+<div><div class="x0">Duty Cycle</div></div>
+
+<div><div class="x1" id="ratioaBtn" onclick="ratioaDef();"></div></div>
+<div><div class="x2" onclick="ratioaDec();">&#8722;</div>
+     <div class="x2" onclick="ratioaInc();">+</div></div>
+<div><div class="x1" id="ratiobBtn" onclick="ratiobDef();"></div></div>
+<div><div class="x2" onclick="ratiobDec();">&#8722;</div>
+     <div class="x2" onclick="ratiobInc();">+</div></div>
 
 </body></html>
 
