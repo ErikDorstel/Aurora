031284633dc27721824041ba7790d6dd2843fedf
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2021-07-26 19:21:30 +0200

    change MIDI output to ledc api

--- MIDI.h
+++ MIDI.h
@@ -4,8 +4,9 @@ APPLEMIDI_CREATE_DEFAULTSESSION_INSTANCE();
 #include "midireceive.h"
 
 void initMIDI() {
-  MIDI.begin();
   MIDI.setHandleNoteOn(midiNoteOn);
-  MIDI.setHandleNoteOff(midiNoteOff); }
+  MIDI.setHandleNoteOff(midiNoteOff);
+  MIDI.begin();
+  ledcSetup(0,1000,8); }
 
 void midiWorker() { MIDI.read(); }
--- midireceive.h
+++ midireceive.h
@@ -1,7 +1,9 @@
 void midiNoteOn(byte channel,byte note,byte velocity) {
-  setSSTC2Tone((pow(2,(float(note)-69)/12))*440,velocity/2,0,0);
-  if (debug) { Serial.println("MIDI Note " + String(note) + " On received."); } }
+  ledcWriteTone(0,(pow(2,(float(note)-69)/12))*440);
+  ledcWrite(0,velocity/8);
+  ledcAttachPin(phaseOut,0);
+  if (debug) { Serial.println("MIDI Note " + String(note) + " On on channel " + String(channel) + " received."); } }
 
 void midiNoteOff(byte channel,byte note,byte velocity) {
-  setSSTC2Tone(0,0,0,0);
-  if (debug) { Serial.println("MIDI Note " + String(note) + " Off received."); } }
+  ledcDetachPin(phaseOut);
+  if (debug) { Serial.println("MIDI Note " + String(note) + " Off on channel " + String(channel) + " received."); } }
