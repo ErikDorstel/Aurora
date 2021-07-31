9862cd3eaf869801ea1c72b2beb391f34f8d809c
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2021-07-31 08:10:08 +0200

    improve pwm generation

--- SSTC-2.h
+++ SSTC-2.h
@@ -1,12 +1,17 @@
 // SSTC2 Library
 // 2021 Erik Dorstel
 // call "initSSTC2();" first
-// call "setSSTC2Para(speed,start,power);" to set parameters for sync mode
-// call "setSSTC2Tone(freqA,ratioA,freqB,ratioB);" to set parameters for async mode
+// call "setSSTC2Para(speed,start,power);" to set parameters for sync mode with timers
+// call "setSSTC2Tone(freqA,ratioA,freqB,ratioB);" to set parameters for async mode with timers
 // call "setSSTC2Sweep(freqA,freqB,duration,ratio);" to set parameters for async mode
+// call "setSSTC2PWM(voice,freq,ratio);" to set parameters for async mode with pwm
 
 const byte onBoardLED=2, phaseIn=18, phaseOut=19; byte SSTC2mode=0;
 hw_timer_t * timer1 = NULL; hw_timer_t * timer2 = NULL; hw_timer_t * timer3 = NULL; hw_timer_t * timer4 = NULL;
+
+byte channels[]={  0,  2,  4,  6,  8, 10, 12, 14};
+byte outGPIOs[]={  4, 13, 16, 17, 21, 22, 23, 25};
+byte inGPIOs[]= { 26, 27, 32, 33, 34, 35, 36, 39};
 
 void IRAM_ATTR onPhase() { detachInterrupt(phaseIn); timerRestart(timer1); timerAlarmEnable(timer1); timerRestart(timer2); timerAlarmEnable(timer2); }
 void IRAM_ATTR onTimerSync1() { attachInterrupt(phaseIn,onPhase,RISING); }
@@ -16,11 +21,18 @@ void IRAM_ATTR onTimerAsync1() { digitalWrite(phaseOut,LOW); digitalWrite(onBoar
 void IRAM_ATTR onTimerAsync2() { digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); }
 void IRAM_ATTR onTimerAsync3() { digitalWrite(phaseOut,LOW); digitalWrite(onBoardLED,HIGH); timerRestart(timer4); timerAlarmEnable(timer4); }
 void IRAM_ATTR onTimerAsync4() { digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); }
+void IRAM_ATTR ioChange() {
+  if (digitalRead(inGPIOs[0])==LOW & digitalRead(inGPIOs[1])==LOW & digitalRead(inGPIOs[2])==LOW & digitalRead(inGPIOs[3])==LOW &
+    digitalRead(inGPIOs[4])==LOW & digitalRead(inGPIOs[5])==LOW & digitalRead(inGPIOs[6])==LOW & digitalRead(inGPIOs[7])==LOW)
+    { digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW); }
+  else { digitalWrite(phaseOut,LOW); digitalWrite(onBoardLED,HIGH); } }
 
 void initSSTC2() {
   pinMode(onBoardLED,OUTPUT); pinMode(phaseIn,INPUT); pinMode(phaseOut,OUTPUT);
   digitalWrite(phaseOut,HIGH); digitalWrite(onBoardLED,LOW);
-  timer1=timerBegin(0,80,true); timer2=timerBegin(1,80,true); timer3=timerBegin(2,80,true); timer4=timerBegin(3,80,true); }
+  timer1=timerBegin(0,80,true); timer2=timerBegin(1,80,true); timer3=timerBegin(2,80,true); timer4=timerBegin(3,80,true);
+  for (byte index=0;index<8;index++) { pinMode(inGPIOs[index],INPUT); pinMode(outGPIOs[index],OUTPUT);
+    digitalWrite(outGPIOs[index],LOW); attachInterrupt(inGPIOs[index],ioChange,CHANGE); } }
 
 void modeSSTC2sync() {
   timerAlarmDisable(timer1); timerAlarmDisable(timer2); timerAlarmDisable(timer3); timerAlarmDisable(timer4); delay(30);
@@ -51,3 +63,7 @@ void setSSTC2Sweep(int freq1, int freq2, int duration, int ratio) {
   int step; if (duration>0) { step=abs(freq1-freq2)*20/duration; } else { step=0; }
   if (freq1<freq2 & step>0) { for (int freq=freq1;freq<=freq2;freq+=step) { setSSTC2Tone(freq,ratio,0,0); delay(20); } setSSTC2Tone(0,0,0,0); }
   if (freq1>freq2 & step>0) { for (int freq=freq1;freq>=freq2;freq-=step) { setSSTC2Tone(freq,ratio,0,0); delay(20); } setSSTC2Tone(0,0,0,0); } }
+
+void setSSTC2PWM(byte voice,int freq,int ratio) {
+  if (freq>0 & ratio>0) { ledcDetachPin(outGPIOs[voice]); ledcSetup(channels[voice],freq,8); ledcWrite(channels[voice],ratio); ledcAttachPin(outGPIOs[voice],channels[voice]); }
+  else { ledcDetachPin(outGPIOs[voice]); } }
--- midireceive.h
+++ midireceive.h
@@ -1,14 +1,5 @@
-byte channels[]={  0,  2,  4,  6,  8, 10, 12, 14};
-byte outGPIOs[]={  4, 13, 16, 17, 21, 22, 23, 25};
-byte inGPIOs[]= { 26, 27, 32, 33, 34, 35, 36, 39};
 byte midiChan[]={  0,  0,  0,  0,  0,  0,  0,  0};
 byte midiNote[]={255,255,255,255,255,255,255,255};
-
-void playTone(byte voice,int tone,byte velocity) {
-  if (tone>0) { ledcWriteTone(channels[voice],tone);
-    ledcWrite(channels[voice],velocity/8);
-    ledcAttachPin(outGPIOs[voice],channels[voice]); }
-  else { ledcDetachPin(outGPIOs[voice]); } }
 
 byte mountVoice(byte channel,byte note) {
   byte voice=255; for (byte index=0;index<8;index++) { if (midiNote[index]==255) { voice=index; break; } }
@@ -20,14 +11,14 @@ byte unmountVoice(byte channel,byte note) {
 
 void midiNoteOn(byte channel,byte note,byte velocity) {
   byte voice=mountVoice(channel,note);
-  if (voice!=255) { playTone(voice,(pow(2,(float(note)-69)/12))*440,velocity); }
+  if (voice!=255) { setSSTC2PWM(voice,(pow(2,(float(note)-69)/12))*440,velocity); }
   if (debug) { Serial.println("MIDI Note " + String(note) + " On on channel " + String(channel) + " received. Play on voice " + String(voice) +"."); } }
 
 void midiNoteOff(byte channel,byte note,byte velocity) {
   byte voice=unmountVoice(channel,note);
-  if (voice!=255) { playTone(voice,0,0); }
+  if (voice!=255) { setSSTC2PWM(voice,0,0); }
   if (debug) { Serial.println("MIDI Note " + String(note) + " Off on channel " + String(channel) + " received. Stop on voice " + String(voice) +"."); } }
 
 void midiControlChange(byte channel,byte data1,byte data2) {
-  if (data1==120 | data1==123) { for (byte voice=0;voice<8;voice++) { playTone(voice,0,0); midiNote[voice]=255; midiChan[voice]=0; }
+  if (data1==120 | data1==123) { for (byte voice=0;voice<8;voice++) { setSSTC2PWM(voice,0,0); midiNote[voice]=255; midiChan[voice]=0; }
     if (debug) { Serial.println("MIDI stop all sounds received."); } } }
